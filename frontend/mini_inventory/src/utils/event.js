408f06c83b4c696a6a11319be0f44008e7493ee2
super <97206982+SuperMo0@users.noreply.github.com>
2026-02-27 22:47:04 +0200

    add date-fns for time formatting

--- frontend/mini_inventory/src/utils/event.js
+++ frontend/mini_inventory/src/utils/event.js
@@ -1,14 +1,17 @@
+import { formatDistanceToNow } from 'date-fns';
 export function formatEvent(event) {
-    const { action, quantity, product_title, source_warehouse_title, destination_warehouse_title } = event;
+    const { action, quantity, product_title, source_warehouse_title, destination_warehouse_title, created_at } = event;
+
+    let timeAgo = formatDistanceToNow(new Date(created_at), { addSuffix: true });
 
     switch (action.toLowerCase()) {
         case 'remove':
-            return `Removed ${quantity}x ${product_title} from ${source_warehouse_title}.`;
+            return `Removed ${quantity}x ${product_title} from ${source_warehouse_title} ${timeAgo}.`;
 
         case 'transfer':
-            return `Transferred ${quantity}x ${product_title} from ${source_warehouse_title} to ${destination_warehouse_title}.`;
+            return `Transferred ${quantity}x ${product_title} from ${source_warehouse_title} to ${destination_warehouse_title} ${timeAgo}.`;
 
         case 'update':
-            return `Updated ${product_title} stock to ${quantity} in ${source_warehouse_title}.`;
+            return `Updated ${product_title} stock to ${quantity} in ${source_warehouse_title} ${timeAgo}.`;
     }
 }
